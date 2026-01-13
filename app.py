4ea6d1a7885c5b27d403c2ca46ee03c3f015b47e
ranjithamstudycenter-maker <ranjithamstudycenter@gmail.com>
2026-01-13 16:03:52 +0530

    app.py

--- app.py
+++ app.py
@@ -51,8 +51,17 @@ def success():
     pdf_name = request.form.get("pdf")
     return send_from_directory(PDF_FOLDER, f"{pdf_name}.pdf", as_attachment=True)
 
-# ----- Render port binding (VERY IMPORTANT) -----
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello World!"
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    print("Starting server on port", port)
+    port = int(os.environ.get("PORT", 4000))
+    print(f"Listening on port {port}")
     app.run(host="0.0.0.0", port=port)
+
