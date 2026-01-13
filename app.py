9ca17f4848c33feedaded3349edb8c72dfbe0041
rscedu <ranjithamstudycenter@gmail.com>
2026-01-13 14:45:19 +0530

    Fix port and add gunicorn

--- app.py
+++ app.py
@@ -58,5 +58,5 @@ def download(pdf_name):
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 5000))  # use Render PORT env variable, default 5000
     app.run(host="0.0.0.0", port=port)
