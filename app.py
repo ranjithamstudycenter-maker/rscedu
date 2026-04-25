8195ead9b38fc6be09041256ff804d863014df4a
ranjithamstudycenter-maker <ranjithamstudycenter@gmail.com>
2026-04-25 16:04:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -169,7 +169,7 @@ def check_demo():
 
     # Admin always gets demo
     if is_admin_phone(phone):
-        return jsonify({"demo_done": {course: True for course in seat_data},  # 🔥 ALL TRUE, "admin": True, "meet_link":meet_link})
+        return jsonify({"demo_done": {course: True for course in seat_data}, "admin": True, "meet_link":meet_link})
 
     demo_done = user["demo_done"].get(course, False)
     return jsonify({"demo_done": demo_done, "meet_link":meet_link})
