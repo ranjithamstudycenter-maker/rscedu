c9763aa3749fdd3644af5e36829aaf8ce4b51b4d
ranjithamstudycenter-maker <ranjithamstudycenter@gmail.com>
2026-09-23 12:14:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6073,9 +6073,8 @@ def practice_session_submit():
         # -------------------------------------------------
 
         conn = sqlite3.connect(
-            "students.db"
+            DB_PATH
         )
-
         conn.row_factory = sqlite3.Row
 
         c = conn.cursor()
